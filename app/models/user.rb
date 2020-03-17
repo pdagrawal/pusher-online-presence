771f258b57b0d8e391d2d1093211cab189629d23
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2020-03-17 12:18:14 +0530

    added pusher for real time update

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
 
   validates :username, presence: :true, uniqueness: { case_sensitive: false }
   validates :is_signed_in, inclusion: [true, false]
+
+  after_create :notify_pusher
+
+  def notify_pusher
+    Pusher.trigger('activity', 'login', self.as_json)
+  end
+
+  def as_json(options={})
+    super(
+      only: [:id, :email, :username]
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  devise_for :users, :controllers => { :sessions => "sessions" }
+  root 'users#index'
 end
