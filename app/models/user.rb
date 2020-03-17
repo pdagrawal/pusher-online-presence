ae3c34425905f5c68ce3f4a53c27f0abddb38c4e
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2020-03-17 11:57:36 +0530

    added device for authentication

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validates :is_signed_in, inclusion: [true, false]
+end
